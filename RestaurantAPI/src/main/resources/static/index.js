114d445f9fca80366cff2243d6b09ca888c04406
Tara Kelly <kellyt2@students.westerntc.edu>
2020-08-06 09:22:07 -0500

    updated project version w/ detailsView and simplified JS

--- RestaurantAPI/src/main/resources/static/index.js
+++ RestaurantAPI/src/main/resources/static/index.js
@@ -1,6 +1,5 @@
 function initialize(){
     getRestaurants("/api/restaurants");  // only need partial URL from inside static folder
-    renderModal("createRestaurant", -1);
 }
 
 function getRestaurants(url){
@@ -53,17 +52,17 @@ function getRestaurantById(id){
 function renderRestaurant(json){
 
     var cardHtml = 
-    `<div id="${json.id}"class="card" height="180">
+    `<div id="${json.id}"class="card" max-height="225">
         <img class="card-img-top" src= ${json.imageURL} alt="Card image" style="width:100%" height="50%">
         <div class="card-body"> 
             <h4 class="card-title">${json.name}</h4>
             <p class="card-text">${json.name} is in the ${json.neighborhood} neighborhood</p>
             <a href="#" id ="btnUpdate" class="btn btn-primary" onclick="renderModal('updateRestaurant', ${json.id})" data-toggle="modal" data-target="#updateRestaurant">UPDATE</a>
             <a href="#" id="btnDelete" class="btn btn-secondary" onclick="deleteRestaurant(${json.id})">DELETE</a>
+            <a href="#" class="btn btn-link" onclick="renderDetailsView('detailsView', ${json.id})" data-toggle="modal" data-target="#detailsView">View Details</a>       
         </div>
     </div>`;
 
-    //document.getElementById("restaurants").insertAdjacentHTML('beforeend', cardHtml);
     return cardHtml;
 
 }
@@ -71,13 +70,13 @@ function renderRestaurant(json){
 function renderManyRestaurants(data){
 
     var jsonArray = JSON.parse(data);
-    console.log(jsonArray);
+
     for(var index = 0; index < jsonArray.length; index++){
         // loop through json array and call renderRestaurant on each object to make a card
         // append cards to card decks
         let json = jsonArray[index];
-         // create card Deck
         var card = renderRestaurant(json);
+        // create card Deck
         var cardDeck;
        
         if(index  == 0 || index % 3 == 0){
@@ -90,18 +89,12 @@ function renderManyRestaurants(data){
         document.getElementById("restaurants").appendChild(cardDeck);
     }
 
-     // From Matt: Once the student cards are created and rendered on our page, we can then find them 
-     // and add on the update buttons with associated modals
-     // TODO: I added the ids to the buttons with each card...will it still work?
-     // Trying to render it with onclick of update button
-    // renderModal("updateRestaurant", "Update a Restaurant");
 }
 
 function renderModal(modalPurpose, id){
 
      //reset modal div every time so don't have to store mult modals in HTML
     document.getElementById("modals").innerHTML = "";
-
     var modalTitle;
 
     if(modalPurpose == "updateRestaurant"){
@@ -111,24 +104,19 @@ function renderModal(modalPurpose, id){
     }
    
    var modalHTML = 
-   
-   // I think adding id to id will fix issue with update modal retaining values
   
     `<div class="modal fade" id="${modalPurpose}">
+        <div class="modal-dialog modal-m">  
 
-        <div class="modal-dialog modal-xl">
-        
-            <div class="modal-content">
-            
+            <div class="modal-content"> 
+
                 <div class="modal-header">
                     <h4 class="modal-title">${modalTitle}</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                
+                </div>     
                 <div class="modal-body">
                     ${renderModalBody(modalPurpose, id)}
                 </div>
-
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="createOrUpdateRestaurant('${modalPurpose}', '${id}')">Save</button>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -137,7 +125,6 @@ function renderModal(modalPurpose, id){
             </div>
 
         </div>
-
     </div`;
 
     document.getElementById("modals").insertAdjacentHTML('beforeend', modalHTML);
@@ -146,101 +133,216 @@ function renderModal(modalPurpose, id){
 
 function renderModalBody(modalPurpose, id){
 
-    var name = "";
-    var address = "";
-    var city = "";
-    var state = "";
-    var zip = "";
-    var neighborhood = "";
-    var delivery = "";
-    var outdoorSeating = "";
-    var imageURL = "";
-
+    // var name = "";
+    // var address = "";
+    // var city = "";
+    // var state = "";
+    // var zip = "";
+    // var neighborhood = "";
+    // var delivery = "";
+    // var outdoorSeating = "";
+    // var imageURL = "";
+    var body = "";
     if(id != -1){
-        restaurant = getRestaurantById(id);
-        input = JSON.parse(restaurant);
-        restaurantID = input.id;
-        name = input.name;
-        address = input.address;
-        city = input.city;
-        state = input.state;
-        zip = input.zip;
-        neighborhood = input.neighborhood;
-        delivery = input.delivery;
-        outdoorSeating = input.delivery;
-        imageURL = input.imageURL;
+        // could do for each prop in restaurant here
+        let restaurant = getRestaurantById(id);
+        let input = JSON.parse(restaurant);
+        let array = Object.entries(input);
+        console.log(array);
+
+        // TODO: issues w/ id showing and naming w/ for in loop
+        // for(property in input){
+        //     var component = renderModalComponent(modalPurpose, property, input[property]);
+        //     body += component;  
+        // }
+
+        //Better way w/ Object.entries:
+        for(var index = 1; index < array.length; index++){
+            var component = renderModalComponent(modalPurpose, array[index][0], array[index][1]);
+            body += component;  
+        }
+        // restaurantID = input.id;
+        // name = input.name;
+        // address = input.address;
+        // city = input.city;
+        // state = input.state;
+        // zip = input.zip;
+        // neighborhood = input.neighborhood;
+        // delivery = input.delivery;
+        // outdoorSeating = input.delivery;
+        // imageURL = input.imageURL;
+    }else{
+        var emptyRestaurant = {
+            id: id,
+            name: "", 
+            address: "", 
+            city: "", 
+            state: "", 
+            zip: "", 
+            neighborhood: "", 
+            delivery: "", 
+            outdoorSeating: "",
+            imageURL: "",
+        }
+        let array = Object.entries(emptyRestaurant);
+        console.log(array);
+
+        for(var index = 1; index < array.length; index++){
+            var component = renderModalComponent(modalPurpose, array[index][0], array[index][1]);
+            body += component;  
+        }
     }
 
     // TODO: can change to something like this
     // if id != -1, for each prop in restaurant, renderTextComponent(modalPurpose, prop, value);
     // and if not -1, make an empty JSON obj with all values empty strings?
 
-    var body = 
+    // var body = 
+    // `<div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-name" class="form-control validate" value="${name}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-name">Restaurant name</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-address" class="form-control validate" value="${address}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-address">Restaurant address</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-envelope prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-city" class="form-control validate" value="${city}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-city">Restaurant city</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-state" class="form-control validate" value="${state}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-state">Restaurant state</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-zip" class="form-control validate" value="${zip}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-zip">Restaurant zip</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-neighborhood" class="form-control validate" value="${neighborhood}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-neighborhood">Restaurant Neighborhood</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-delivery" class="form-control validate" value="${delivery}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-delivery">Delivery</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-outdoorSeating" class="form-control validate" value="${outdoorSeating}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-outdoorSeating">Outdoor Seating</label>
+    // </div>
+
+    // <div class="md-form mb-5">
+    //     <i class="fas fa-user prefix grey-text"></i>
+    //     <input type="text" id="${modalPurpose}-imageURL" class="form-control validate" value="${imageURL}">
+    //     <label data-error="wrong" data-success="right" for="${modalPurpose}-imageURL">Restaurant image URL</label>
+    // </div>`;
+
+    return body;
+}
+
+function renderModalComponent(modalPurpose, property, value){
+
+    let component = 
     `<div class="md-form mb-5">
         <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-name" class="form-control validate" value="${name}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-name">Restaurant name</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-address" class="form-control validate" value="${address}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-address">Restaurant address</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-envelope prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-city" class="form-control validate" value="${city}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-city">Restaurant city</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-state" class="form-control validate" value="${state}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-state">Restaurant state</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-zip" class="form-control validate" value="${zip}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-zip">Restaurant zip</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-neighborhood" class="form-control validate" value="${neighborhood}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-neighborhood">Restaurant Neighborhood</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-delivery" class="form-control validate" value="${delivery}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-delivery">Delivery</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-outdoorSeating" class="form-control validate" value="${outdoorSeating}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-outdoorSeating">Outdoor Seating</label>
-    </div>
-
-    <div class="md-form mb-5">
-        <i class="fas fa-user prefix grey-text"></i>
-        <input type="text" id="${modalPurpose}-imageURL" class="form-control validate" value="${imageURL}">
-        <label data-error="wrong" data-success="right" for="${modalPurpose}-imageURL">Restaurant image URL</label>
+        <input type="text" id="${modalPurpose}-${property}" class="form-control validate" value="${value}">
+        <label data-error="wrong" data-success="right" for="${modalPurpose}-${property}">Restaurant ${property}</label>
     </div>`;
 
+    return component;
+}
+
+function renderDetailsView(modalId, id){
+
+    // add link in each card to render details
+    document.getElementById("modals").innerHTML = "";
+    var restaurant = getRestaurantById(id);
+    var json = JSON.parse(restaurant);
+
+    var detailsView = 
+    `<div id="${modalId}" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-xl">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    ${renderDetailsViewBody(json)}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>`;
+
+    document.getElementById("modals").insertAdjacentHTML('beforeend', detailsView);
+
+}
+
+function renderDetailsViewBody(json){
+
+    var delivery = "no";
+    var outdoorSeating = "no";
+
+    if(json.delivery == true){
+        delivery = "yes";
+    }
+    if(json.outdoorSeating == true){
+        outdoorSeating = "yes";
+    }
+
+    var body = 
+    `<div class="card">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="${json.imageURL}" class="card-img" alt="mexican food">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${json.name}</h5>
+              <dl class="row ml-lg-0 ml-3">
+                  <dt class="col-sm-6">Address:</dt>
+                  <dd class="col-sm-6">${json.address}</dd>
+                  <dt class="col-sm-6">City:</dt>
+                  <dd class="col-sm-6">${json.city}</dd>
+                  <dt class="col-sm-6">Neighborhood:</dt>
+                  <dd class="col-sm-6">${json.neighborhood}</dd>
+                  <dt class="col-sm-6">Delivery:</dt>
+                  <dd class="col-sm-6">${delivery}</dd>
+                  <dt class="col-sm-6">Outdoor Seating:</dt>
+                  <dd class="col-sm-6">${outdoorSeating}</dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+    </div>`;
+    
     return body;
 }
 
 // use this to to a POST request
 function createOrUpdateRestaurant(modalPurpose, id){
 
-    console.log(modalPurpose);
-    console.log(`${modalPurpose}-name`);
    var sendData = {
-       // like this to get data entered in modal form
-       //TODO is this OK for post?
        "id": id,
        "name": document.getElementById(`${modalPurpose}-name`).value,
        "address": document.getElementById(`${modalPurpose}-address`).value,
@@ -280,7 +382,7 @@ function sendPut(sendData){
             getRestaurants("/api/restaurants");
         }
     };
-    // Be sure that the JSON student is coverted to String before sending, using JSON.stringify
+    
     xhttp.send(JSON.stringify(sendData));
 }
 
